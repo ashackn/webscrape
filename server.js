ee0f3404c08d76617437a5eb573938ff3deb1172
ashackn <ashackn@gmail.com>
2019-03-15 17:22:29 -0600

    changes

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("./models");
 
-// var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT ||3000;
 
 // Initialize Express
 var app = express();
@@ -183,8 +183,8 @@ app.delete("/articles/:id", function(req, res){
 
    
     //  Start the server
-    //  app.listen(PORT, function() {
-    //    console.log("App running on port " + PORT + "!");
-    //  });
+     app.listen(PORT, function() {
+       console.log("App running on port " + PORT + "!");
+     });
     
     
